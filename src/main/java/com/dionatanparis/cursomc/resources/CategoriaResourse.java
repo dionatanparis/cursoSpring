0a5c6573f04b05de91f89d19cefcd0d1f60b6b39
dionatan paris <dionatan.paris@gmail.com>
2018-04-17 22:39:40 -0300

    Primeira classe de dominio

---
+++ src/main/java/com/dionatanparis/cursomc/resources/CategoriaResourse.java
@@ -0,0 +1,28 @@
+package com.dionatanparis.cursomc.resources;
+
+
+import com.dionatanparis.cursomc.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResourse {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar(){
+
+        Categoria cat1  = new Categoria(1,"informática");
+        Categoria cat2  = new Categoria(2,"Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
+    }
+
+}
